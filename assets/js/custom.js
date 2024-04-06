7de52a1333f92f66a71b9c2ff3ebb14605d41142
Emanuel Costa <emanuel.ccti@gmail.com>
2024-04-06 18:57:10 -0300

    marcação de pontos

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,50 +6,60 @@ chave_pix = '(85)98925-2964';
 descricao_pix = 'Letícia Lima e Silva - Itaú Unibanco S.A';
 link_whatsapp = 'https://api.whatsapp.com/send/?phone=5585996500294&text&type=phone_number&app_absent=0';
 
-$(document).ready(function() {
+$(document).ready(function () {
 
   $('#text_rifa').html(text);
-    $('a.chave_pix').attr('data-clipboard-text',chave_pix);
-    $('span.chave_pix').html(chave_pix);
-    $('#tipo_chave_pix').html(tipo_chave_pix);
-    $('#descricao_pix').html(descricao_pix);
-    $('#link_whatsapp').attr('href',link_whatsapp);
+  $('a.chave_pix').attr('data-clipboard-text', chave_pix);
+  $('span.chave_pix').html(chave_pix);
+  $('#tipo_chave_pix').html(tipo_chave_pix);
+  $('#descricao_pix').html(descricao_pix);
+  $('#link_whatsapp').attr('href', link_whatsapp);
 
-    clipboard = new ClipboardJS('.btn');
-    clipboard.on('success', function(e) {
-      alert('Copiado');
-      // TODO Toast em botão
-      // console.info('Action:', e.action);
-      // console.info('Text:', e.text);
-      // console.info('Trigger:', e.trigger);
-      e.clearSelection();
-    });
-
-    clipboard.on('error', function(e) {
-      console.log(e);
-      console.error('Action:', e.action);
-      console.error('Trigger:', e.trigger);
-    });
-
-    var pontos_vendidos = [
-      // 104,
-      // 123,
-      // 135,
-      // 144,
-    ];
-
-    $.ajax({
-      url: 'pontos_vendidos.json',
-      success: function(data){
-        // console.log(data);
-        var div_pontos = $('#div_pontos')
-        for (i = 1; i <= 160; i++) {
-          div_pontos.append('<div class="col-md-2 col-lg-1 col-sm-2 com-xs-2 border border-secondary col-1 m-2 p-2 d-flex justify-content-center">' +
-            '<span> ' + ((data.includes(i)) ? '  <span class="badge bg-black font-weight-bold rounded-pill"><i class="fa fa-check "></i></span>' + '</span>' : i) +
-            '</div>')
-        }
-      }
-    })
-
-
+  clipboard = new ClipboardJS('.btn');
+  clipboard.on('success', function (e) {
+    alert('Copiado');
+    // TODO Toast em botão
+    // console.info('Action:', e.action);
+    // console.info('Text:', e.text);
+    // console.info('Trigger:', e.trigger);
+    e.clearSelection();
   });
+
+  clipboard.on('error', function (e) {
+    console.log(e);
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+  });
+
+  var pontos_vendidos = [
+    // 104,
+    // 123,
+    // 135,
+    // 144,
+  ];
+
+  var url = "https://script.google.com/macros/s/AKfycbzn1b9vKuUNLbMg7JQ2TPjiRQNnC0G8sfw6lDOAcImpDv_NtS1-mksP0bkZC8mqdpPo/exec?callback=ctrlq&name="
+  var name = "Amit Agarwal"
+
+  var request = jQuery.ajax({
+    crossDomain: true,
+    url: url + encodeURIComponent(name),
+    method: "GET",
+    dataType: "jsonp"
+  });
+
+  // $.ajax({
+  //   url: 'pontos_vendidos.json',
+  //   success: function (data) {
+  //     // console.log(data);
+  //     var div_pontos = $('#div_pontos')
+  //     for (i = 1; i <= 160; i++) {
+  //       div_pontos.append('<div class="col-md-2 col-lg-1 col-sm-2 com-xs-2 border border-secondary col-1 m-2 p-2 d-flex justify-content-center">' +
+  //         '<span> ' + ((data.includes(i)) ? '  <span class="badge bg-black font-weight-bold rounded-pill"><i class="fa fa-check "></i></span>' + '</span>' : i) +
+  //         '</div>')
+  //     }
+  //   }
+  // })
+
+
+});
