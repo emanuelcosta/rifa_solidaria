be499ba8e7d094fbaf018e8f581bb53671a8287e
Emanuel Costa <emanuel.ccti@gmail.com>
2024-04-06 15:04:20 -0300

    commit inicial - prototipo incial

---
+++ CAMPOS BANCO DE DADOS.md
@@ -0,0 +1,18 @@
+Rifa
+- Titulo
+- Descricao
+- Data de sorteio (Texto)
+- Quantidade de números
+- Tipo de chave pix
+- Chave pix
+- Descricao de chave pix
+- Whatsapp
+
+
+Rifa_numeros
+- Codigo da rifa
+- Numero
+- Nome
+- Telefone de contato
+- Pago?
+- anexo comprovante (2ª fase)
---
+++ custom.js
@@ -0,0 +1,77 @@
+text = "Oi gente, tudo bem? Me chamo Letícia e estou fazendo essa rifa para conseguir realizar uma cirurgia que custa R$ 15 mil reais. Fui diagnosticada com um tumor na coxa e preciso muito de sua ajuda para que minha saúde seja restaurada. Agradeço muito a todos que decidirem me ajudar. Abraço a todos e que Deus abençoe sua vida!";
+app_name = 'Rifa Solidária';
+number_points = 160;
+tipo_chave_pix = 'Chave PIX(Telefone)';
+chave_pix = '(85)98925-2964';
+descricao_pix = 'Letícia Lima e Silva - Itaú Unibanco S.A';
+link_whatsapp = 'https://api.whatsapp.com/send/?phone=5585996500294&text&type=phone_number&app_absent=0';
+
+$(document).ready(function() {
+    clipboard = new ClipboardJS('.btn');
+    clipboard.on('success', function(e) {
+      alert('Copiado');
+      // TODO Toast em botão
+      // console.info('Action:', e.action);
+      // console.info('Text:', e.text);
+      // console.info('Trigger:', e.trigger);
+      e.clearSelection();
+    });
+
+    clipboard.on('error', function(e) {
+      console.error('Action:', e.action);
+      console.error('Trigger:', e.trigger);
+    });
+
+    var pontos_vendidos = [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      9,
+      11,
+      12,
+      14,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      29,
+      30,
+      33,
+      35,
+      37,
+      41,
+      46,
+      53,
+      64,
+      65,
+      67,
+      72,
+      83,
+      // 90,
+      // 96,
+      // 100,
+      // 102,
+      // 104,
+      // 123,
+      // 135,
+      // 144,
+    ];
+
+    var div_pontos = $('#div_pontos')
+    for (i = 1; i <= 160; i++) {
+      div_pontos.append('<div class="col-md-2 col-lg-1 col-sm-2 com-xs-2 border border-secondary col-1 m-2 p-2 d-flex justify-content-center">' +
+        '<span> ' + ((pontos_vendidos.includes(i)) ? '  <span class="badge bg-black font-weight-bold rounded-pill"><i class="fa fa-check "></i></span>' + '</span>' : i) +
+        '</div>')
+    }
+
+  });
